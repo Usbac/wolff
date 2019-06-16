d375c0dfb1a7823c82bbee6156375f87c5aae993
Alejandro <castillo.alex97@outlook.com>
2019-06-15 23:15:11 -0400

    Updated version tags

--- system/config.php
+++ system/config.php
@@ -17,7 +17,7 @@ define('CONFIG', [
     'public_dir'    => $root . 'public/',
 
     //General
-    'version'   => '1.2.1',
+    'version'   => '1.3',
     'start'     => microtime(true),
     'title'     => 'Wolff',
     'main_page' => 'main_page',
