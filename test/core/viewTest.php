611511566771fb7e903b68d10f3f956eb3aa5151
alejandro <castillo.alex97@outlook.com>
2020-08-26 18:14:01 -0400

    Updated Wolff\Core\View test

--- test/core/viewTest.php
+++ test/core/viewTest.php
@@ -9,7 +9,9 @@ class ViewTest extends TestCase
 {
 
     const VIEW_NAME = 'phpunit_testing_view';
+    const VIEW_NAME_2 = 'phpunit_testing_view_ext';
     const FILE = '../app/views/' . self::VIEW_NAME . '.wlf';
+    const FILE_2 = '../app/views/' . self::VIEW_NAME_2 . '.html';
     const CONTENT = '<h1>{! $msg !}</h1><br/>';
     const CONTENT_RENDERED = '<h1>Hello world</h1><br/>';
 
@@ -27,6 +29,10 @@ class ViewTest extends TestCase
         $view_file = fopen(self::FILE, "w") or die();
         fwrite($view_file, self::CONTENT);
         fclose($view_file);
+
+        $view_file_2 = fopen(self::FILE_2, "w") or die();
+        fwrite($view_file_2, self::CONTENT);
+        fclose($view_file_2);
     }
 
 
@@ -36,11 +42,17 @@ class ViewTest extends TestCase
         $this->assertFalse(View::exists($this->non_existent));
         $this->assertEquals(self::CONTENT, View::getSource(self::VIEW_NAME, $this->data, false));
         $this->assertEquals(self::CONTENT_RENDERED, View::getRender(self::VIEW_NAME, $this->data, false));
+
+        $this->assertTrue(View::exists(self::VIEW_NAME_2 . '.html'));
+        $this->assertFalse(View::exists(self::VIEW_NAME_2));
+        $this->assertEquals(self::CONTENT, View::getSource(self::VIEW_NAME_2 . '.html', $this->data, false));
+        $this->assertEquals(self::CONTENT_RENDERED, View::getRender(self::VIEW_NAME_2 . '.html', $this->data, false));
     }
 
 
-    public function tearDown():void
+    public function tearDown(): void
     {
         unlink(self::FILE);
+        unlink(self::FILE_2);
     }
 }
