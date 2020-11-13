9ea13cbd01647663c8b98615874f2b8867275c8e
alejandro <castillo.alex97@outlook.com>
2020-11-13 17:57:17 -0400

    Moved the standard library initialization to the project files

--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php
 
 require '../vendor/autoload.php';
-require '../system/web.php';
 
 $config = require '../system/config.php';
+if ($config['stdlib_on']) {
+    include_once '../vendor/usbac/wolff-framework/src/stdlib.php';
+}
+
+require '../system/web.php';
 
 $wolff = new Wolff\Kernel($config);
 $wolff->start();
