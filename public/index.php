1672a30edd9fd55d826e9edf5ea53cb1b4e79080
alejandro <castillo.alex97@outlook.com>
2020-11-16 01:01:27 -0400

    Moved the error reporting configuration to the 'public/index.php' file

--- public/index.php
+++ public/index.php
@@ -2,10 +2,22 @@
 
 require '../vendor/autoload.php';
 
+/**
+ * Config
+ */
+
 $config = require '../system/config.php';
+
 if ($config['stdlib_on']) {
     include_once '../vendor/usbac/wolff-framework/src/stdlib.php';
 }
+
+error_reporting($config['development_on'] ? E_ALL : 0);
+ini_set('display_errors', strval($config['development_on']));
+
+/**
+ * Wolff
+ */
 
 require '../system/web.php';
 
